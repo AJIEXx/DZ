96c7018d4bf5ba9cf7c2b8d05733b8e7b041adf5
ZF-1000 <n.shanin@mail.ru>
2022-08-06 15:39:21 +0300

    add GetArrayLimitLength and PrintArray functions

---
+++ final_task/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше
+либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Пример:
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> []
+*/
+
+
+// Первоначальный массив будем задавать на старте выполнения алгоритма.
+
+using System;
+using static System.Console;
+Clear();
+
+string[] array = {"hello", "2", "world", ":-)"};
+
+Write("[");
+PrintArray(array);
+Write("]");
+
+Write(" -> ");
+
+Write("[");
+PrintArray(GetArrayLimitLength(array));
+Write("]");
+
+
+
+
+string[] GetArrayLimitLength(string[] arr)
+{
+    string[] res = new string[arr.Length];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+            {
+                res[j] = arr[i];
+                j++;
+            }
+    }
+    return res;
+}
+
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Write($"{arr[i]}");
+        if (i != arr.Length - 1) Write(", ");
+    }
+}
